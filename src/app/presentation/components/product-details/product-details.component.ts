1a22b84576ec616f08f9b1759b5b7aa6fc9f3d86
Roman Franco Valero Calle <romanfranco767@gmail.com>
2025-05-25 08:21:44 -0400

    fix: resolviendo stop propagations

--- src/app/presentation/components/product-details/product-details.component.ts
+++ src/app/presentation/components/product-details/product-details.component.ts
@@ -1,14 +1,24 @@
 import {Component, inject} from '@angular/core';
 import {ProductModel} from '../../../core/models/product.model';
 import {ProductRepository} from '../../../data/repositories/product.repository';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router';
 import {ProductCardComponent} from '../product-card/product-card.component';
 import {CartStore} from '../../state/cart.store';
+import {MatAnchor} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-details',
   imports: [
-    ProductCardComponent
+    ProductCardComponent,
+    MatAnchor,
+    RouterLinkActive,
+    RouterLink,
+    MatAnchor,
+    RouterLinkActive,
+    MatAnchor,
+    MatIcon,
+    RouterLinkActive
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
